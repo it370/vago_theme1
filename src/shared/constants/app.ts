2840b066a277601f24780c111b464857e03e3df8
nelvin-hmar <109198308+nelvin-tf@users.noreply.github.com>
2026-03-24 00:46:05 +0530

    -- appname from .env

--- src/shared/constants/app.ts
+++ src/shared/constants/app.ts
@@ -1,4 +1,6 @@
-export const APP_NAME = "VAGO";
-export const APP_TAGLINE = "Curated fashion for the discerning individual.";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "VAGO";
+export const APP_TAGLINE =
+  process.env.NEXT_PUBLIC_APP_TAGLINE ||
+  "Curated fashion for the discerning individual.";
 export const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "https://vago-admin.vercel.app";
